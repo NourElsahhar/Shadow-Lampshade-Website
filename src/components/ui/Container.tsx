729b28f48a8e3533adfbaa11c8a9a8e63277f3cd
Nour Elsahhar <165329803+NourElsahhar@users.noreply.github.com>
2026-07-02 01:57:38 +0300

    chore: modify project structure

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -8,6 +8,6 @@ export default function Container({ children }: ContainerProps) {
   return (
     // We limit the content width to improve readability
     // on ultra-wide screens.
-    <div className="mx-auto w-full max-w-7xl px-6 lg:px-8">{children}</div>
+    <div className="mx-auto w-full max-w-\[1280px]\ px-6 lg:px-8">{children}</div>
   );
 }
